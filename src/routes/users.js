97218b52c3c8cc56625b18709cfb5ae4891b2b71
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-11-12 13:57:24 +0300

    add auth logic & routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
 const { createObjectCsvWriter } = require('csv-writer');
 
--- src/server.js
+++ src/server.js
@@ -5,19 +5,70 @@ const dotenv = require('dotenv');
 const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
+const usersRouter = require('./routes/users');
 
 dotenv.config();
 
-const contactsRoute = require('./routes/users');
 
 const app = express();
 const PORT = process.env.PORT || 6000;
-const csvFilePath = path.join(__dirname, 'users.csv');
+const csvFilePath = path.join(__dirname, '../data/users.csv');
 
 app.use(cors());
+// Middleware for JSON parsing
 app.use(express.json());
+//Connecting a route to save users
+app.use('/api/users', usersRouter);
 
-app.use('/api', contactsRoute);
+// Stub for admin data
+const admin = {
+    email: 'admin@example.com',
+    passwordHash: bcrypt.hashSync('password123', 10), // захэшированный пароль
+};
+
+// Token generation
+const generateToken = (email) => {
+    return jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+};
+
+
+// Middleware for token verification
+const authenticateJWT = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) return res.status(401).json({ message: 'No token provided' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if (err) return res.status(403).json({ message: 'Invalid token' });
+        req.user = user;
+        next();
+    });
+};
+
+// Login route
+app.post('/api/admin/login', (req, res) => {
+    const { email, password } = req.body;
+
+    if (email === admin.email && bcrypt.compareSync(password, admin.passwordHash)) {
+        const token = generateToken(email);
+        return res.json({ token });
+    } else {
+        return res.status(401).json({ message: 'Invalid credentials' });
+    }
+});
+
+// CSV download route (secure)
+app.get('/api/admin/download-csv', authenticateJWT, (req, res) => {
+    if (fs.existsSync(csvFilePath)) {
+        res.download(csvFilePath, 'users.csv');  // File name
+    } else {
+        res.status(404).json({ message: 'CSV file not found' });
+    }
+});
+
+// Route for HTML page with CSV download button
+app.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/download-page.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
