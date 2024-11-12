5222abdbe68122df9f95d2b458271a92a0a68458
i.shablovskiy <ilya.shablovskiy@gmail.com>
2024-11-12 13:35:31 +0300

    change server structure & add routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const { createObjectCsvWriter } = require('csv-writer');
 
 const router = express.Router();
+const csvFilePath = path.join(__dirname, '../../data/users.csv');
 
 const csvWriter = createObjectCsvWriter({
-    path: './data/users.csv',
+    path: csvFilePath,
     header: [
         { id: 'email', title: 'Email' },
         { id: 'password', title: 'Password' },
@@ -17,14 +19,16 @@ const csvWriter = createObjectCsvWriter({
 router.post('/save-user', async (req, res) => {
     const {  email, password, phone } = req.body;
 
+    if (!email || !password) {
+        return res.status(400).json({ message: 'Email and password are required' });
+    }
+
     try {
         await csvWriter.writeRecords([{ email, password, phone }]);
-        res.setHeader('Content-Disposition', 'attachment; filename=users.csv');
-        res.setHeader('Content-Type', 'text/csv');
-        fs.createReadStream('./data/users.csv').pipe(res);
+        res.status(200).json({ message: 'User saved successfully' });
     } catch (error) {
         console.error('Error writing to CSV:', error);
-        res.status(500).send('Server error');
+        res.status(500).json({ message: 'Failed to save user data' });
     }
 });
 
--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const dotenv = require('dotenv');
+const path = require('path');
+const fs = require('fs');
 const cors = require('cors');
-const contactsRoute = require('./routes/users');
 
-require('dotenv').config();
+dotenv.config();
+
+const contactsRoute = require('./routes/users');
 
 const app = express();
 const PORT = process.env.PORT || 6000;
+const csvFilePath = path.join(__dirname, 'users.csv');
 
 app.use(cors());
 app.use(express.json());
